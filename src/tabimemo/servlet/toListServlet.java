87c86c0ed87f65290d76d9cf53d7549c173f75ee
Raomatsu <100784895+Raomatsu@users.noreply.github.com>
2022-03-07 02:12:12 +0900

    Add files via upload

--- src/tabimemo/servlet/toListServlet.java
+++ src/tabimemo/servlet/toListServlet.java
@@ -13,14 +13,6 @@ import tabimemo.logic.LocationDAO;
 import tabimemo.object.Location;
 
 public class toListServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-
-    public toListServlet() {
-        super();
-
-    }
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -29,19 +21,16 @@ public class toListServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String area = request.getParameter("area");
-//		System.out.println(area);
 
 		//該当地方の全都道府県(の入ったString配列)をリクエストスコープに格納
 		String[] prefectures =
 				tabimemo.object.PrefectureList.areaAndPrefectures.get(area);
-//		System.out.println(prefectures[0]);
 		request.setAttribute("allPrefecturesInTheArea", prefectures);
 
 		//該当地方の各都道府県についての全レコードをリクエストスコープに格納
 		LocationDAO lDao = new LocationDAO();
 		lDao.connectToDB();
 		List<Location> locations = lDao.getPrefecturesInf(area);
-//		System.out.println(locations.get(0));
 		request.setAttribute("AllLocationsInfInTheArea", locations);
 
 		//list.jspに遷移
